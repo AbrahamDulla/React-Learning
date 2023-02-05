e1f0abf8d636728d77d7ec50c12ba23bc2199c11
Abraham Dulla <abrish.d143t@gmail.com>
2023-02-05 19:24:28 +0300

    app bar

--- src/Components/AppBar.jsx
+++ src/Components/AppBar.jsx
@@ -2,7 +2,11 @@ import "./AppBar.css";
 import React from "react";
 
 const AppBar = () => {
-  return <div className="appBar"></div>;
+  return (
+    <div className="appBar">
+      <h1 className="upbar">This is app bar</h1>
+    </div>
+  );
 };
 
 export default AppBar;
