b185b840235ef2f0e8a0cb7076db224eb0ee5ced
Abraham Dulla <abrish.d143t@gmail.com>
2023-02-17 20:50:50 +0300

    axios learning

--- src/Components/login.jsx
+++ src/Components/login.jsx
@@ -7,14 +7,32 @@ import { React } from "react";
 
 const Hello = () => {
   return (
-    <div className="login">
-      <h4 style={{ paddingTop: 15 }}>Login Form</h4>
-      <div className="form-body">
-        <p>email</p>
-        <input style={{ width: 100 }} type="email" />
-        <p>password</p>
-        <input style={{ width: 100 }} type="password" />
-        <button>Login</button>
+    <div style={{ display: "grid", paddingTop: 20 }}>
+      <div className="login">
+        <h4 style={{ paddingTop: 15 }}>Login Form</h4>
+        <div className="form-body">
+          <div style={{ display: "flex" }}>
+            <label style={{ marginRight: 49 }}>email</label>
+            <input style={{ width: 140, height: 20 }} type="email" />
+          </div>
+          <div style={{ display: "flex", marginTop: 20 }}>
+            <label style={{ marginRight: 20 }}>password</label>
+            <input style={{ width: 140, height: 20 }} type="password" />
+          </div>
+          <br />
+          <button style={{ marginTop: 15, height: 32 }}>Login</button>
+        </div>
+      </div>
+
+      <br />
+
+      <div className="login">
+        <h4 style={{ paddingTop: 10 }}>Login Form</h4>
+        <div className="form-body" style={{ paddingTop: 1 }}>
+          <div style={{ display: "flex" }}>
+            <label style={{}}>Body</label>
+          </div>
+        </div>
       </div>
     </div>
   );
